a0ebc88bccfacf9044b8a8d9169333fac38fc3b5
spidycoder <imh10028.21@bitmesra.ac.in>
2023-11-25 11:19:19 +0530

    fixed some errors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,37 +1,52 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./Components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   darkMode: "class",
   theme: {
     screens: {
-      'sm': {'min': '360px', 'max': '767px'},
+      sm: { min: "360px", max: "767px" },
       // => @media (min-width: 640px and max-width: 767px) { ... }
 
-      'md': {'min': '768px', 'max': '1023px'},
+      md: { min: "768px", max: "1023px" },
       // => @media (min-width: 768px and max-width: 1023px) { ... }
 
-      'lg': {'min': '1024px', 'max': '1279px'},
+      lg: { min: "1024px", max: "1279px" },
       // => @media (min-width: 1024px and max-width: 1279px) { ... }
 
-      'xl': {'min': '1280px', 'max': '1535px'},
+      xl: { min: "1280px", max: "1535px" },
       // => @media (min-width: 1280px and max-width: 1535px) { ... }
 
-      '2xl': {'min': '1536px'},
+      "2xl": { min: "1536px" },
       // => @media (min-width: 1536px) { ... }
     },
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        playfair: "Playfair Display, serif",
+        hind: "Hind, sans-serif",
+        poppins: "Poppins, sans-serif",
+        oswald: "Oswald, sans-serif",
+      },
+      fontSize: {
+        "4.5xl": "2.5rem",
+      },
+      colors: {
+        modal: "rgb(0, 0, 0, 0.9)",
+        gravy: "#FFFFFF80",
+        opa: "rgb(0, 0, 0, 0.5)",
+        dark: "#777777",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
